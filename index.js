a168d5f906200975a6777af44c8ce60ff8b3969b
Caelin Sutch <caelinsutch@gmail.com>
2020-01-25 10:57:23 -0800

    Tested

--- index.js
+++ index.js
@@ -8,11 +8,8 @@ const pathToScript = '~/Github/scripts/shutdown.sh';
 
 
 app.get('/bottomside-shutdown', (req, res) => {
-  if (shell.exec(pathToScript).code !== 0) {
-    res.status(400).send("Something went wrong!");
-  } else {
-    res.status(200).send("Bottomside shutdown!");
-  }
+  shell.exec(pathToScript);
+  res.status(200).send("Bottomside shutdown!");
 });
 
 app.listen(port, () => console.log(`Shell script server listening on port ${port}!`))
